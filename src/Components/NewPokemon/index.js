dc3ae889dc276412f5d99154d913cd6c99c2da63
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-05-31 20:46:12 -0500

    Input range tooltip

--- src/Components/NewPokemon/index.js
+++ src/Components/NewPokemon/index.js
@@ -1,77 +1,80 @@
-import './style.css'
+import "./style.css";
 
 const NewPokemon = () => {
+  return (
+    <div className="new_pokemon_container">
+      <span className="new_pokemon_title">Nuevo Pokemon</span>
 
-    return (
-        <div className="new_pokemon_container" >
-        <span className="new_pokemon_title">Nuevo Pokemon</span>
-
-        <div className="new_pokemon_input_container">
+      <div className="new_pokemon_input_container">
+        <div className="side_container">
+          <div className="new_pokemon_one_input_container">
+            <label>Nombre:</label>
+            <input type="text" name="nombre" />
+          </div>
+          <div className="new_pokemon_one_input_container">
+            <label>Imagen:</label>
+            <input type="text" name="imagen" />
+          </div>
+        </div>
 
         <div className="side_container">
           <div className="new_pokemon_one_input_container">
-          <label>Nombre:</label>
-          <input type="text" name="nombre" />
-          </div>  
-          <div className="new_pokemon_one_input_container">
-          <label>Imagen:</label>
-          <input type="text" name="imagen" />
-          </div>  
-        </div>
-        
-        
-        <div className="side_container">
-          <div className="new_pokemon_one_input_container">
-          <label>Ataque:</label>
-          <div className="new_pokemon_one_range">
-          <span>
-            1
-          </span>
-          <input type="range" min="1" max="100" className="new_pokemon_range"/>
-          <span>
-            100
-          </span>
+            <label>Ataque:</label>
+            <div className="new_pokemon_one_range">
+              <span>1</span>
+              <div class="tooltip">
+                <input
+                  type="range"
+                  min="1"
+                  max="100"
+                  title="30"
+                  alt="30"
+                  className="new_pokemon_range"
+                />
+
+                <span class="tooltiptext">Tooltip text</span>
+              </div>
+              <span>100</span>
+            </div>
           </div>
-
-          </div>  
           <div className="new_pokemon_one_input_container">
-          <label>Defensa:</label>
-          <div className="new_pokemon_one_range">
-          <span>
-            1
-          </span>
-          <input type="range" min="1" max="100" className="new_pokemon_range"/>
-          <span>
-            100
-          </span>
+            <label>Defensa:</label>
+            <div className="new_pokemon_one_range">
+              <span>1</span>
+              <div class="tooltip">
+                <input
+                  type="range"
+                  min="1"
+                  max="100"
+                  title="30"
+                  alt="30"
+                  className="new_pokemon_range"
+                />
+
+                <span class="tooltiptext">Tooltip text</span>
+              </div>
+              <span>100</span>
+            </div>
           </div>
-
-          </div>  
         </div>
+      </div>
 
-        </div>
-
-        <div className="action_button_container">
-            <button type="button" className="newbutton action_button">
-            <div className="newbutton_icon">
+      <div className="action_button_container">
+        <button type="button" className="newbutton action_button">
+          <div className="newbutton_icon">
             <ion-icon name="save-outline"></ion-icon>
-            </div>
-            <div className="newbutton_text">
-              Guardar
-            </div>
-            </button>
-            <button type="button" className="newbutton action_button">
-            <div className="newbutton_icon">
+          </div>
+          <div className="newbutton_text">Guardar</div>
+        </button>
+        <button type="button" className="newbutton action_button">
+          <div className="newbutton_icon">
             <ion-icon name="close-outline"></ion-icon>
-            </div>
-            <div className="newbutton_text">
-              Cancelar
-            </div>
-            </button>
-        </div>
-
-        </div>
-    )
-}
+          </div>
+          <div className="newbutton_text">Cancelar</div>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default NewPokemon;
